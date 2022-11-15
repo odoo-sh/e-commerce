c5918e0b0f1d0f464b354e0e19d0acc4b5547b16
Logeshkumar <logeshkumar@sodexis.com>
2022-11-15 15:41:20 +0530

    [ID: 25557 ] Migrate website_sale_charge_payment_fee - Corrections

--- website_sale_charge_payment_fee/static/src/js/website_sale_fee.js
+++ website_sale_charge_payment_fee/static/src/js/website_sale_fee.js
@@ -29,7 +29,7 @@ odoo.define("website_sale_change_payment_fee.website_sale_fee", function (requir
                 }
                 checked_pm_id = $(this).val();
             }
-            if (selected_acquirer !== clicked_acquirer  && (window.location.href.indexOf("/my/orders/") == -1)) {
+            if (selected_acquirer !== clicked_acquirer  && (window.location.href.indexOf("/shop/") != -1)) {
                 // Follow standard logic disabling the "Pay Now" button
                 // https://github.com/odoo/odoo/blob/c2cce50f75096fcc9f940d16bb0ecf40a2899d60/addons/website_sale_delivery/static/src/js/website_sale_delivery.js#L56
                 $("#o_payment_form_pay").prop("disabled", true);
